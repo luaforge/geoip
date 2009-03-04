26ae3aed1b7cdd4ccf1ee20e3da278d19effd6de
Wolfgang Oertl <wiz@luaforge.net>
2009-03-04 11:55:56 +0000

    - code cleanup - iteration on a result object is possible - catch stderr messages when opening the database file - use GEOIP_INDEX_CACHE instead of GEOIP_MEMORY_CACHE

--- geoip/geoip.c
+++ geoip/geoip.c
@@ -1,35 +1,38 @@
-// vim:sw=4:sts=4
-/**
+/** vim:sw=4:sts=4
+ *
  * Bindings for the GeoIP database library by MaxMind.
  * by Wolfgang Oertl 2009
- *
- * Revisions:
- *  2009-03-02	first version.
- *
- * Installation:
- *  To compile, you need the headers GeoIP.h, GeoIPCity.h and maybe others,
- *  which are contained in the C library that can be downloaded from MaxMind.
- *
- *  The library libGeoIP.so must be available.  If you already have
- *  libGeoIP.so.1 or similar, create a symbolic link.  Otherwise compile
- *  the downloaded library and install it.
- *
- *  Finally: gcc -shared -o geoip.so geoip.c -lGeoIP.  You could also use
- *  the static version of libGeoIP to avoid a runtime dependency.
- *
- * Example Usage:
- *  geoip = require("geoip")
- *  g = geoip.open_type "country"
- *  r = g:lookup("74.125.67.100")
- *  r = g:lookup("google.com")
- *  print(r)
- *  print(r.country_code)
- *
  */
 
 #include <GeoIPCity.h>
 #include <lua.h>
 #include <lauxlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+/**
+ * The "Field" structure describes one field of the result with name and
+ * how to extract the data.
+ */
+struct result_t;
+typedef struct _field_t {
+    const char *name;		/* name of the field */
+    int mode;
+    int offset;
+    int (*callback)(lua_State *L, struct result_t *r, struct _field_t *f);
+} Field;
+
+
+/**
+ * One such structure exists per database type.
+ */
+typedef struct {
+    Field *fields;	/* array of field definitions */
+    void (*gc)(lua_State *L, struct result_t *r);
+    int (*tostring)(lua_State *L, struct result_t *r);
+} ResultMeta;
+
 
 /**
  * Result is a structure containing all the information on a location
@@ -38,11 +41,9 @@
  * Depending on the database used, one of the fields is filled in.  Most of the
  * fields can be read with the appropriate __index method.
  */
-
-typedef struct {
-    GeoIPRecord *gir;	/* for the city edition, the complete record */
-    int id;		/* for the country edition, the location id */
-    GeoIPRegion *reg;	/* for the country edition, the region */
+typedef struct result_t {
+    ResultMeta *meta;
+    void *data;		/* some token returned by libGeoIP */
 } Result;
 
 #define RESULT	"GeoIPResult"
@@ -62,125 +63,160 @@ static int _push_opt_string(lua_State *L, const char *s)
     return 0;
 }
 
-
-/**
- * The result is a GeoIPCity record.  Access all these fields.
- */
-static int _record_index_city(lua_State *L, GeoIPRecord *g, const char *field)
+static int _access_field(lua_State *L, Result *r, Field *f)
 {
-    if (!field) {
-	lua_pushfstring(L, "%s, %s (%s)", g->city, g->country_name,
-	    g->country_code);
-	return 1;
-    }
+    char *p = (char*) r->data;
 
-    if (!strcmp(field, "city"))
-	return _push_opt_string(L, g->city);
-    if (!strcmp(field, "postal_code"))
-	return _push_opt_string(L, g->postal_code);
-    if (!strcmp(field, "latitude")) {
-	lua_pushnumber(L, g->latitude);
+    switch (f->mode) {
+	case 1:
+	return _push_opt_string(L, * (char**) (p + f->offset));
+	
+	case 2:
+	lua_pushnumber(L, * (float*) (p + f->offset));
 	return 1;
+	
+	// callback
+	case 3:
+	return f->callback(L, r, f);
     }
-    if (!strcmp(field, "longitude")) {
-	lua_pushnumber(L, g->longitude);
-	return 1;
-    }
-    if (!strcmp(field, "country"))
-	return _push_opt_string(L, g->country_name);
-    if (!strcmp(field, "country_code"))
-	return _push_opt_string(L, g->country_code);
-    if (!strcmp(field, "region"))
-	return _push_opt_string(L, g->region);
-    if (!strcmp(field, "region_name"))
-	return _push_opt_string(L, GeoIP_region_name_by_code(g->country_code,
-	    g->region));
-    if (!strcmp(field, "time_zone"))
-	return _push_opt_string(L, GeoIP_time_zone_by_country_and_region(
-	    g->country_code, g->region));
-    if (!strcmp(field, "continent"))
-	return _push_opt_string(L, g->continent_code);
 
     return 0;
 }
 
 
-/**
- * The result is from the country edition.  Provide access to the results.
- */
-static int _record_index_country(lua_State *L, Result *g,
-    const char *field)
-{
-    int id = g->id;
+/* --------- city database ------------ */
 
-    if (!field) {
-	lua_pushfstring(L, "%s (%s)",
-	    GeoIP_name_by_id(id),
-	    GeoIP_code_by_id(id));
-	return 1;
+static int city_region_name(lua_State *L, Result *r, Field *f)
+{
+    GeoIPRecord *g = (GeoIPRecord*) r->data;
+    return _push_opt_string(L, GeoIP_region_name_by_code(
+	g->country_code, g->region));
+}
+
+static int city_time_zone(lua_State *L, Result *r, Field *f)
+{
+    GeoIPRecord *g = (GeoIPRecord*) r->data;
+    return _push_opt_string(L, GeoIP_time_zone_by_country_and_region(
+	g->country_code, g->region));
+}
+
+static Field city_fields[] = {
+    { "city", 1, offsetof(GeoIPRecord, city) },
+    { "postal_code", 1, offsetof(GeoIPRecord, postal_code) },
+    { "latitude", 2, offsetof(GeoIPRecord, latitude) },
+    { "longitude", 2, offsetof(GeoIPRecord, longitude) },
+    { "country", 1, offsetof(GeoIPRecord, country_name) },
+    { "country_code", 1, offsetof(GeoIPRecord, country_code) },
+    { "region", 1, offsetof(GeoIPRecord, region) },
+    { "continent", 1, offsetof(GeoIPRecord, continent_code) },
+    { "region_name", 3, 0, city_region_name },
+    { "time_zone", 3, 0, city_time_zone },
+    { NULL, 0, 0 },
+};
+
+static int city_tostring(lua_State *L, Result *r)
+{
+    GeoIPRecord *g = (GeoIPRecord*) r->data;    
+    lua_pushfstring(L, "%s, %s (%s)", g->city, g->country_name,
+	g->country_code);
+    return 1;
+}
+
+static void city_gc(lua_State *L, Result *r)
+{
+    if (r->data) {
+	GeoIPRecord_delete((GeoIPRecord*) r->data);
+	r->data = NULL;
     }
-
-    if (!strcmp(field, "country"))
-	return _push_opt_string(L, GeoIP_name_by_id(id));
-    if (!strcmp(field, "country_code"))
-	return _push_opt_string(L, GeoIP_code_by_id(id));
-    if (!strcmp(field, "continent"))
-	return _push_opt_string(L, GeoIP_continent_by_id(id));
-
-    return 0;
 }
 
+/* ----------- country database ----------- */
 
-/**
- * The result is from the region edition.
- */
-static int _record_index_region(lua_State *L, Result *g, const char *field)
+typedef const char *(*country_func)(int id);
+
+static country_func country_funcs[] = {
+    GeoIP_name_by_id,
+    GeoIP_code_by_id,
+    GeoIP_continent_by_id
+};
+
+static int country_field_access(lua_State *L, Result *r, Field *f)
 {
-    GeoIPRegion *reg = g->reg;
+    int id = (int) r->data;
+    return _push_opt_string(L, country_funcs[f->offset](id));
+}
 
-    if (!field) {
-	lua_pushfstring(L, "%s, %s", reg->region,
-	    reg->country_code);
-	return 1;
+static Field country_fields[] = {
+    { "country", 3, 0, country_field_access },
+    { "country_code", 3, 1, country_field_access },
+    { "continent", 3, 2, country_field_access },
+    { NULL },
+};
+
+static int country_tostring(lua_State *L, Result *r)
+{
+    int id = (int) r->data;
+    lua_pushfstring(L, "%s (%s)", GeoIP_name_by_id(id), GeoIP_code_by_id(id));
+    return 1;
+}
+
+static void country_gc(lua_State *L, Result *r)
+{
+}
+
+/* ---------- region database ----------- */
+
+static int region_time_zone(lua_State *L, Result *r, Field *f)
+{
+    GeoIPRegion *g = (GeoIPRegion*) r->data;
+    return _push_opt_string(L, GeoIP_time_zone_by_country_and_region(
+	g->country_code, g->region));
+}
+
+static Field region_fields[] = {
+    { "country_code", 1, offsetof(GeoIPRegion, country_code) },
+    { "region", 1, offsetof(GeoIPRegion, region) },
+    { "time_zone", 3, 0, region_time_zone },
+    { NULL },
+};
+
+static int region_tostring(lua_State *L, Result *r)
+{
+    GeoIPRegion *reg = (GeoIPRegion*) r->data;
+    lua_pushfstring(L, "%s, %s", reg->region, reg->country_code);
+    return 1;
+}
+
+static void region_gc(lua_State *L, Result *r)
+{
+    if (r->data) {
+	GeoIPRegion_delete((GeoIPRegion*)r->data);
+	r->data = NULL;
     }
-
-    if (!strcmp(field, "country_code"))
-	return _push_opt_string(L, reg->country_code);
-    if (!strcmp(field, "region"))
-	return _push_opt_string(L, reg->region);
-    if (!strcmp(field, "time_zone"))
-	return _push_opt_string(L, GeoIP_time_zone_by_country_and_region(
-	    reg->country_code, reg->region));
-
-    return 0;
 }
 
 
-/**
- * Call the appropriate handler depending on the type of the result.
- */
-static int _result_index_switch(lua_State *L, const char *field)
-{
-    Result *gir = (Result*) luaL_checkudata(L, 1, RESULT);
+/* --------------------------------------- */
 
-    if (gir->gir)
-	return _record_index_city(L, gir->gir, field);
-    else if (gir->id)
-	return _record_index_country(L, gir, field);
-    else if (gir->reg)
-	return _record_index_region(L, gir, field);
-
-    return 0;
-}
-
+ResultMeta
+    result_meta_city = { city_fields, city_gc, city_tostring },
+    result_meta_country = { country_fields, country_gc, country_tostring },
+    result_meta_region = { region_fields, region_gc, region_tostring };
 
 /**
  * Handle accesses to fields.
  */
 static int l_result_index(lua_State *L)
 {
-    const char *field = luaL_checkstring(L, 2);
-    return _result_index_switch(L, field);
+    Result *r = (Result*) luaL_checkudata(L, 1, RESULT);
+    const char *name = luaL_checkstring(L, 2);
+
+    Field *f;
+    for (f=r->meta->fields; f->name; f++)
+	if (!strcmp(f->name, name))
+	    return _access_field(L, r, f);
+    
+    return 0;
 }
 
 
@@ -190,36 +226,58 @@ static int l_result_index(lua_State *L)
  */
 static int l_result_tostring(lua_State *L)
 {
-    return _result_index_switch(L, NULL);
+    Result *r = (Result*) luaL_checkudata(L, 1, RESULT);
+    return r->meta->tostring(L, r);
+}
+
+static int l_result_gc(lua_State *L)
+{
+    Result *r = (Result*) luaL_checkudata(L, 1, RESULT);
+    r->meta->gc(L, r);
+    return 0;
 }
 
 
 /**
- * Garbage collect a result.
+ * Use the result as iterator; return all the fields and values in a loop
+ * of this type: for name, value in r do print(name, value) end
+ *
+ * @param r  The result
+ * @param nil  always nil
+ * @param name  Name of last accessed field, or nil if first call.
  */
-static int l_result_gc(lua_State *L)
+static int l_result_call(lua_State *L)
 {
-    Result *gir = (Result*) luaL_checkudata(L, 1, RESULT);
-    if (gir->gir) {
-	GeoIPRecord_delete(gir->gir);
-	gir->gir = NULL;
+    Result *r = luaL_checkudata(L, 1, RESULT);
+    Field *f = r->meta->fields;
+
+    // if not the first call, find the last accessed field, then advance to the
+    // next field.
+    if (lua_type(L, 3) == LUA_TSTRING) {
+	const char *name = lua_tostring(L, 3);
+	while (f->name && strcmp(f->name, name))
+	    f ++;
+	if (f->name)
+	    f++;
     }
-    if (gir->reg) {
-	GeoIPRegion_delete(gir->reg);
-	gir->reg = NULL;
-    }
-    return 0;
+
+    if (!f->name)
+	return 0;
+
+    lua_pushstring(L, f->name);
+    return 1 + _access_field(L, r, f);
 }
 
 static const luaL_Reg result_methods[] = {
     { "__tostring", l_result_tostring },
     { "__gc", l_result_gc },
     { "__index", l_result_index },
+    { "__call", l_result_call },
     { NULL, NULL }
 };
 
 
-/* ----- GeoIP ----- */
+/* ----------------- GeoIP ---------------- */ 
 
 typedef struct {
     GeoIP *gi;
@@ -232,9 +290,9 @@ typedef struct {
  *
  * @param gi  GeoIP object
  * @param name  Host name or IP address to look up
- * @return  On success, a GeoIPRecord is returned, else nil.
+ * @return  On success, a Result object is returned, else nil.
  */
-static int l_by_name(lua_State *L)
+static int l_geoip_lookup(lua_State *L)
 {
     lua_geoip *lgi = (lua_geoip*) luaL_checkudata(L, 1, GEOIP);
     Result gir, *p;
@@ -247,21 +305,24 @@ static int l_by_name(lua_State *L)
 	int id = GeoIP_id_by_name(lgi->gi, hostname);
 	if (!id)
 	    return 0;
-	gir.id = id;
+	gir.meta = &result_meta_country;
+	gir.data = (void*) id;
 	break;
 
 	case GEOIP_REGION_EDITION_REV1:;
 	GeoIPRegion *reg = GeoIP_region_by_name(lgi->gi, hostname);
 	if (!reg)
 	    return 0;
-	gir.reg = reg;
+	gir.meta = &result_meta_region;
+	gir.data = (void*) reg;
 	break;
 
 	case GEOIP_CITY_EDITION_REV1:;
 	GeoIPRecord *r = GeoIP_record_by_name(lgi->gi, hostname);
 	if (!r)
 	    return 0;
-	gir.gir = r;
+	gir.meta = &result_meta_city;
+	gir.data = (void*) r;
 	break;
 
 	default:
@@ -306,17 +367,21 @@ static int l_geoip_gc(lua_State *L)
 static const luaL_Reg geoip_methods[] = {
     { "__tostring", l_geoip_tostring },
     { "__gc", l_geoip_gc },
-    { "lookup", l_by_name },
+    { "lookup", l_geoip_lookup },
     { NULL, NULL }
 };
 
 
-static int _open_common(lua_State *L, GeoIP *gi)
+/**
+ * A GeoIP state has been created; now set up a userdata with metatable
+ * to let Lua scripts access it.
+ */
+static int _open_common(lua_State *L, GeoIP *gi, const char *err_msg)
 {
     lua_geoip *lgi;
 
     if (!gi)
-	return 0;
+	return luaL_error(L, err_msg);
 
     lgi = (lua_geoip*) lua_newuserdata(L, sizeof(*lgi));
     lgi->gi = gi;
@@ -333,26 +398,74 @@ static int _open_common(lua_State *L, GeoIP *gi)
 
 
 /**
- * Open a GeoIP database file by specifying the desired type.  The default
- * file name for this type will be used.
+ * libGeoIP prints error messages (e.g. failure to open a data file) on stderr
+ * instead of returning it somehow.  To avoid messy output, redirect stderr
+ * to a pipe, which is read after the operation.
+ */
+typedef struct {
+    int old_stderr;
+    int pipefd[2];
+    char buf[200];
+} Stderr;
+
+/* set up a pipe to catch stderr */
+static void stderr_init(Stderr *e)
+{
+    pipe(e->pipefd);
+    fcntl(e->pipefd[0], F_SETFL, O_NONBLOCK);
+    fcntl(e->pipefd[1], F_SETFL, O_NONBLOCK);
+    e->old_stderr = dup(2);
+    dup2(e->pipefd[1], 2);
+}
+
+/* read the error message(s) and restore the original stderr */
+static void stderr_done(Stderr *e)
+{
+    int n = read(e->pipefd[0], e->buf, sizeof(e->buf));
+    if (n >= 0)
+	e->buf[n] = 0;
+
+    close(e->pipefd[0]);
+    close(e->pipefd[1]);
+    dup2(e->old_stderr, 2);
+}
+
+
+/**
+ * Open a GeoIP database file by specifying the desired type(s).  The default
+ * file name for each type will be used.  The first successfully opened
+ * database is used.  If none of the types are available, raise an error.
  *
- * @param type  The database type to open.  can be "city" or "country".
+ * @param type...  The database type(s) to open.
  * @return  A GeoIP object or nil on error.
  */
 static int l_open_type(lua_State *L)
 {
-    const char *type_name = luaL_checkstring(L, 1);
+    Stderr e;
+    int i;
     GeoIPDBTypes type;
+    GeoIP *gi = NULL;
 
-    if (!strcmp(type_name, "city"))
-	type = GEOIP_CITY_EDITION_REV1;
-    else if (!strcmp(type_name, "country"))
-	type = GEOIP_COUNTRY_EDITION;
-    else
-	return luaL_error(L, "invalid type (city or country)");
+    stderr_init(&e);
 
-    GeoIP *gi = GeoIP_open_type(type, GEOIP_MEMORY_CACHE);
-    return _open_common(L, gi);
+    for (i = 1; i<=lua_gettop(L); i++) {
+	const char *type_name = luaL_checkstring(L, i);
+
+	if (!strcmp(type_name, "city"))
+	    type = GEOIP_CITY_EDITION_REV1;
+	else if (!strcmp(type_name, "country"))
+	    type = GEOIP_COUNTRY_EDITION;
+	else if (!strcmp(type_name, "region"))
+	    type = GEOIP_REGION_EDITION_REV1;
+	else
+	    return luaL_error(L, "invalid type (city, country or region)");
+
+	if ((gi = GeoIP_open_type(type, GEOIP_INDEX_CACHE)))
+	    break;
+    }
+
+    stderr_done(&e);
+    return _open_common(L, gi, e.buf);
 }
 
 
@@ -365,8 +478,12 @@ static int l_open_type(lua_State *L)
 static int l_open(lua_State *L)
 {
     const char *filename = luaL_checkstring(L, 1);
-    GeoIP *gi = GeoIP_open(filename, GEOIP_MEMORY_CACHE);
-    return _open_common(L, gi);
+    Stderr e;
+
+    stderr_init(&e);
+    GeoIP *gi = GeoIP_open(filename, GEOIP_INDEX_CACHE);
+    stderr_done(&e);
+    return _open_common(L, gi, e.buf);
 }
 
 
